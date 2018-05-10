0a5492389325b7d6892164494f1be6b12c451d64
WongHoyu <HoyuWong@163.com>
2018-05-10 09:05:26 +0800

    添加全局数据(假数据)

---
+++ 任务提醒软件/任务提醒软件/model(数据模型)/TypeItem.swift
@@ -0,0 +1,25 @@
+//
+//  TypeItem.swift
+//  任务提醒软件
+//
+//  Created by WongHoyu on 2018/5/10.
+//  Copyright © 2018年 hwanghaoyu. All rights reserved.
+//
+
+import UIKit
+
+class TypeItem: NSObject {
+    //任务分类
+    var name:String = ""
+    
+    //分类图标
+    var icon:String = "提醒"
+    
+    //构造方法
+    init(name:String) {
+        super.init()
+        self.name = name
+    }
+    
+    
+}
--- 任务提醒软件/任务提醒软件/type(任务种类模块相关的文件)/TypeViewController.swift
+++ 任务提醒软件/任务提醒软件/type(任务种类模块相关的文件)/TypeViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class TypeViewController: UITableViewController {
+    
+    //模拟数据
+    var typeList = [TypeItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("Hello swift")
+        onCreateData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,24 +25,36 @@ class TypeViewController: UITableViewController {
 
     //设置tableView的行数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //返回10条数据
-        return 10
+        //返回数据条数
+        return typeList.count
     }
     
     //设置cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //获取行对应的数据
+        let typeItem = typeList[indexPath.row]
+        
         //为cell设置Identifier
         let cellIdentifier = "typeCell"
         
         //依据Identifer创建Cell
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier )
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
         
         //设置cell的标题
         cell.textLabel?.text = "任务分类:\(indexPath.row)"
         
         //设置cell的缩略图
-        cell.imageView?.image = UIImage(named: "心情")
+        cell.imageView?.image = UIImage(named: typeItem.icon)
         
         return cell
+    }
+    
+    /// 创建模拟数据
+    func onCreateData() {
+        for i in 1...10 {
+            let name = "任务: \(i)"
+            let type = TypeItem(name: name)
+            typeList.append(type)
+        }
     }
 }
