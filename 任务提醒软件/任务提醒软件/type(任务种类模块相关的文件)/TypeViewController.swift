fae1efd20e05a2ac8b8ba0a2b7c4e29817a2aecb
WongHoyu <HoyuWong@163.com>
2018-05-10 08:36:46 +0800

    将“任务分类”界面与TypeViewController绑定

---
+++ 任务提醒软件/任务提醒软件/type(任务种类模块相关的文件)/TypeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TypeViewController.swift
+//  任务提醒软件
+//
+//  Created by WongHoyu on 2018/5/10.
+//  Copyright © 2018年 hwanghaoyu. All rights reserved.
+//
+
+import UIKit
+
+class TypeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hello swift")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
