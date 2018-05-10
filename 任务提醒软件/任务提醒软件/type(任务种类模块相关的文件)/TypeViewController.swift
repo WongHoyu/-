3cb37d615fd3bb700ad6671cde84c08672b0528f
WongHoyu <HoyuWong@163.com>
2018-05-10 08:48:48 +0800

    给任务分类界面设置假数据

--- 任务提醒软件/任务提醒软件/type(任务种类模块相关的文件)/TypeViewController.swift
+++ 任务提醒软件/任务提醒软件/type(任务种类模块相关的文件)/TypeViewController.swift
@@ -21,5 +21,26 @@ class TypeViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //设置tableView的行数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //返回10条数据
+        return 10
+    }
     
+    //设置cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //为cell设置Identifier
+        let cellIdentifier = "typeCell"
+        
+        //依据Identifer创建Cell
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier )
+        
+        //设置cell的标题
+        cell.textLabel?.text = "任务分类:\(indexPath.row)"
+        
+        //设置cell的缩略图
+        cell.imageView?.image = UIImage(named: "心情")
+        
+        return cell
+    }
 }
