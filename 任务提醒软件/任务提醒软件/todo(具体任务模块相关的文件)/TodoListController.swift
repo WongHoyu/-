01b5d4697c401e838fafee4878e5db691a98b120
WongHoyu <HoyuWong@163.com>
2018-05-15 15:09:49 +0800

    重新设计任务清单UI，增加一个CheckBox，点击响应的cell，进行勾选与取消勾选

--- 任务提醒软件/任务提醒软件/todo(具体任务模块相关的文件)/TodoListController.swift
+++ 任务提醒软件/任务提醒软件/todo(具体任务模块相关的文件)/TodoListController.swift
@@ -33,11 +33,42 @@ class TodoListController: UITableViewController {
         //获取cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         
-        //设置标题内容
-        cell.textLabel?.text = item?.text
+        //获取label
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        //设置label内容
+        label.text = item?.text
         
         return cell
+    }
+    
+    //设置check勾选
+    func onCheckMark(cell:UITableViewCell, item:TodoItem) {
+        //根据Tag获取cell中的checkbox
+        let check = cell.viewWithTag(1002) as! UIImageView
         
+        //通过item中的checked属性来控制勾号是否显示
+        if item.checked {
+            check.image = UIImage(named: "checkbox-checked")
+        } else {
+            check.image = UIImage(named: "checkbox-normal")
+        }
+    }
+    
+    //点击cell的响应方法
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //获取row数据
+        let item = todoList?.items[indexPath.row]
+        
+        //check属性取反
+        item?.onChangeChecked()
+        
+        //设置cell的勾选框
+        let cell = tableView.cellForRow(at: indexPath)
+        onCheckMark(cell: cell!, item: item!)
+        
+        //取消选中状态
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
