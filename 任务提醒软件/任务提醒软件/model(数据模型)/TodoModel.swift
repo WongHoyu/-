f81e2a46271cb37c8166b3be242c3514dc95ea9f
WongHoyu <HoyuWong@163.com>
2018-05-26 20:33:35 +0800

    新增读取本地数据

--- 任务提醒软件/任务提醒软件/model(数据模型)/TodoModel.swift
+++ 任务提醒软件/任务提醒软件/model(数据模型)/TodoModel.swift
@@ -15,22 +15,25 @@ class TodoModel: NSObject {
     //构造方法
     override init() {
         super.init()
+        print("沙盒文件夹路径: \(documentsDirectory())")
+        print("数据文件路径: \(dataFilePath())")
         //初始化模拟数据
-        onCreateData()
+        //onCreateData()
+        loadData()
     }
     
-    /// 创建模拟数据
-    func onCreateData() {
-        for i in 1...10 {
-            let name = "任务: \(i)"
-            let type = TypeItem(name: name)
-            for j in 0...4 {
-                let todo = TodoItem(text: "任务清单: \(j)", checked: false, dueDate: Date(), shouldRemind: true, level: 0)
-                type.items.append(todo)
-            }
-            typeList.append(type)
-        }
-    }
+//    /// 创建模拟数据
+//    func onCreateData() {
+//        for i in 1...10 {
+//            let name = "任务: \(i)"
+//            let type = TypeItem(name: name)
+//            for j in 0...4 {
+//                let todo = TodoItem(text: "任务清单: \(j)", checked: false, dueDate: Date(), shouldRemind: true, level: 0)
+//                type.items.append(todo)
+//            }
+//            typeList.append(type)
+//        }
+//    }
     
     /// 获取沙盒路径
     func documentsDirectory() -> String {
@@ -58,7 +61,28 @@ class TodoModel: NSObject {
         data.write(toFile: dataFilePath(), atomically: true)
     }
     
-    
+    func loadData() {
+        //获取本地数据文件地址
+        let path = self.dataFilePath()
+        //声明文件管理器
+        let defaultManager = FileManager()
+        //通过文件地址判断数据文件是否存在
+        if defaultManager.fileExists(atPath: path) {
+            //读取文件数据
+            let data = NSData(contentsOfFile: path)
+            //解码器
+            let unarchiver = NSKeyedUnarchiver(forReadingWith: data! as Data)
+            //通过归档时设置的关键字checklist还原liats
+            typeList = unarchiver.decodeObject(forKey: "todolist") as! Array
+            //结束解码
+            unarchiver.finishDecoding()
+        } else {
+            //如果文件不存在，则是第一次安装该应用，创建一个checklist
+            let type = TypeItem(name: "女神有关")
+            typeList.append(type)
+            saveData()
+        }
+    }
     
     class func nextTodoItemId() -> Int {
         return 0
